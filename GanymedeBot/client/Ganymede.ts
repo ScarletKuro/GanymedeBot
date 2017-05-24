7d7f3eae27585ee7845570ebb8f3aaa08daa78a7
ScarletKuro <wordkap@gmail.com>
2017-05-24 07:17:22 -0700

    Rework
    Modifications:
    Using the modular yamdbf framework for Discord
    Using now typescript

---
+++ GanymedeBot/client/Ganymede.ts
@@ -0,0 +1,67 @@
+import { Client, ListenerUtil, LogLevel } from "yamdbf";
+import { TextChannel, RichEmbed, Message, Guild } from "discord.js";
+
+const config: any = require("../config.json");
+const pkg: any = require("../package.json");
+
+const { on, once } = ListenerUtil;
+
+export class Ganymede extends  Client {
+
+    public config: any;
+    public constructor() {
+        super({
+            name: "Ganymedebot",
+            token: config.token,
+            owner: config.owner,
+            version: pkg.version,
+            unknownCommandError: false,
+            statusText: "Under | Development",
+            readyText: "Ready\u0007",
+            commandsDir: "./commands",
+            ratelimit: "10/1m",
+            pause: true,
+            logLevel: LogLevel.INFO
+        });
+        
+        this.config = config;
+    }
+
+    @once('pause')
+    private async _onPause(): Promise<void> {
+        await this.setDefaultSetting('prefix', '%');
+        await this.setDefaultSetting('cases', 0);
+        this.emit("continue");
+    }
+
+    @once('clientReady')
+    private async _onClientReady(): Promise<void> {
+        
+    }
+
+    @on('command')
+    private _logCommand(name: string, args: any, execTime: number, message: Message): Promise<Message> {
+        const logChannel: TextChannel = <TextChannel>this.channels.get(this.config.commands);
+        const embed: RichEmbed = new RichEmbed()
+            .setColor(11854048)
+            .setAuthor(`${message.author.tag} (${message.author.id})`, message.author.avatarURL);
+        if (message.guild) embed.addField('Guild', message.guild.name, true);
+        embed.addField('Exec time', `${execTime.toFixed(2)}ms`, true)
+            .addField('Command content', message.content)
+            .setFooter(message.channel.type.toUpperCase(), this.user.avatarURL)
+            .setTimestamp();
+        return logChannel.send({ embed: embed });
+    }
+
+    @on('guildCreate')
+    @on('guildDelete', false)
+    private _logGuild(guild: Guild, joined: boolean = true): Promise<Message> {
+        const logChannel: TextChannel = <TextChannel>this.channels.get(this.config.guilds);
+        const embed: RichEmbed = new RichEmbed()
+            .setColor(joined ? 8450847 : 13091073)
+            .setAuthor(`${guild.name} (${guild.id})`, guild.iconURL)
+            .setFooter(joined ? 'Welcome to the Animosity server.' : 'Left guild')
+            .setTimestamp();
+        return logChannel.sendEmbed(embed);
+    }
+}
---
+++ GanymedeBot/commands/8Ball.ts
@@ -0,0 +1,46 @@
+﻿import { Collection, RichEmbed } from "discord.js";
+import { Client, Command, GuildStorage, Message } from "yamdbf";
+
+const responses: string[] = [
+    "Yes",
+    "No",
+    "Maybe",
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most Likely",
+    "Outlook good",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My soures say no",
+    "Outlook not so good",
+    "Very doubtful"
+];
+
+export default class EightBall extends Command<Client>
+{
+    public constructor(client: Client) {
+        super(client, {
+            name: "8ball",
+            description: "Let the 8ball preict your future",
+            aliases: ['8'],
+            usage: `<prefix>8ball`,
+            extraHelp: "",
+            group: "base"
+        });
+    }
+
+    public async action(message: Message, args: string[]): Promise<any> {
+        if (args.length > 1)
+        message.reply(`${responses[Math.floor(Math.random() * responses.length)]}`);
+    }
+}
